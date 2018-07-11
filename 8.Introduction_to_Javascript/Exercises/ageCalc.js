8f590dc32afa587dacff401a34359bd2c21fac37
Farhan Ahmed <ahmed-farhan@hotmail.com>
2018-07-11 19:01:00 -0400

    Completed section 8

---
+++ 8.Introduction_to_Javascript/Exercises/ageCalc.js
@@ -0,0 +1,6 @@
+var age = prompt("Enter your age");
+
+var leapYears = age / 4;
+var daysAlive = age * 365 + leapYears;
+
+alert(age + " years is rougly " + daysAlive + " days");
---
+++ 8.Introduction_to_Javascript/Exercises/stalker.js
@@ -0,0 +1,6 @@
+var fName = prompt("What is your first name?");
+var lName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your full name is: " + fName + " " + lName);
+console.log("Your age is: " + age)
